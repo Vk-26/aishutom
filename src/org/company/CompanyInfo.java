80b10b4b3154a32be36bbd3a502a595235a6e718
Vasanth <vasantharun95@gmail.com>
2021-07-22 12:44:39 +0530

    hi

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,11 @@
+package org.company;
+
+public class CompanyInfo {
+
+	public static void main(String[] args) {
+		System.out.println("companyName(Greens Technology)");
+		System.out.println("companyId(1111)");
+		System.out.println("companyId(OMR)");
+	}
+
+}
